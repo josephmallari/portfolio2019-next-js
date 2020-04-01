d37a8995cc0df291022ecc3f0d9d130d58e85e50
josephmallari <joseph.mallari48@gmail.com>
2020-04-01 19:18:27 +0200

    add meta og tags

--- components/IndexHead.js
+++ components/IndexHead.js
@@ -6,7 +6,10 @@ function indexHead() {
       <Head>
         <title>Joseph Mallari's Website</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property='og:image' content='/static/chrome.png"/>
+        <meta property="og:image" content="/static/chrome.png" />
+        <meta property="og:title" content="Joseph Mallari's portfolio" />
+        <meta property="og:description" content="Portfolio of Joseph Mallari" />
+        <meta property="og:url" content="https://josephmallari.de" />
         <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
     </div>
